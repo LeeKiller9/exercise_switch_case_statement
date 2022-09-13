53cdaba0418626d122a528327a8eef477b8d400d
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-09-13 09:19:44 +0700

    up index styles js files

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function check_month() {
+    let day = null
+    let month = document.getElementById('month').value
+    if (month === '' || month < 1 || month > 12) {
+        alert('Wrong data')
+    } else {
+        month = parseInt(month)
+        switch (month) {
+            case 2:
+                day = 28
+                break
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                day = 30
+                break
+            default:
+                day = 31
+        }
+    }
+    document.getElementById('result').innerHTML = 'Month ' + month + ' have ' + day + ' Day'
+}
